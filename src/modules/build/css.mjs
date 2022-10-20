67e7856070efa6b6598ac9a5fbb45c942a404ea3
Gregory Wild-Smith <gregory.wild-smith@sironamedical.com>
2022-10-20 13:52:33 -0700

    updating css deps

--- src/modules/build/css.mjs
+++ src/modules/build/css.mjs
@@ -2,17 +2,23 @@ import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
 import allFS from "fs";
 import postcss from "postcss";
+// The below are a replacement for the abandoned precss module
+import postcssAdvancedVariables from "postcss-advanced-variables";
+import postcssAtroot from "postcss-atroot";
+import postcssExtendRule from "postcss-extend-rule";
+import postcssNested from "postcss-nested";
 import postcssPresetEnv from "postcss-preset-env";
-import precss from "precss";
+import postcssPropertyLookup from "postcss-property-lookup";
 
 const fs = allFS.promises;
+const precss = [postcssExtendRule, postcssAdvancedVariables, postcssPresetEnv, postcssAtroot, postcssPropertyLookup, postcssNested]; // Plugin
 
 const compressCSS = async(from, to) => {
 	const css = await fs.readFile(from);
 	const result = await postcss([
 		postcssPresetEnv,
 		autoprefixer,
-		precss,
+		...precss,
 		cssnano
 	])
 		.process(css, { from, to });
